52b54281f46c700b6b23d0e1f8be023667e8074c
Grin <nguyenvanvietthang1307@gmail.com>
2026-05-20 17:01:08 +0700

    fixing bug

--- coregame/scenes/player/swim.gd
+++ coregame/scenes/player/swim.gd
@@ -8,14 +8,14 @@ var bobbing_amount: float = 5.0
 var idle_water_time: float = 0.0
 var time_to_float: float = 3.0 
 
-# --- Thêm biến đếm thời gian giữ phím cho trục Y ---
+# Thêm biến đếm thời gian giữ phím cho trục Y
 var y_input_timer: float = 0.0
-var time_to_move_y: float = 0. # Thời gian kích hoạt (1 giây)
+var time_to_move_y: float = 1.0 # Thời gian kích hoạt (1 giây)
 
 func _enter() -> void:
 	obj.change_animation("swim")
 	timer = 0.0 
-	idle_water_time = 0.0 
+	idle_water_time = 0.0  
 	y_input_timer = 0.0 # Reset bộ đếm khi vào nước
 
 func _update(delta: float) -> void:
@@ -36,20 +36,20 @@ func _update(delta: float) -> void:
 		idle_water_time = 0.0
 		timer = 0.0 
 
-	# --- Logic đếm thời gian cho trục Y ---
+	#  Logic đếm thời gian cho trục Y 
 	if abs(dir_y) > 0.1:
 		y_input_timer += delta
 	else:
 		y_input_timer = 0.0 # Buông phím ra là reset ngay lập tức
 
-	# --- TRỤC NGANG (X) ---
+	# TRỤC NGANG X
 	if abs(dir_x) > 0.1:
 		obj.change_direction(sign(dir_x))
 		obj.velocity.x = move_toward(obj.velocity.x, dir_x * swim_speed, 20)
 	else:
 		obj.velocity.x = move_toward(obj.velocity.x, 0, 10)
 
-	# --- TRỤC DỌC (Y) ---
+	# TRỤC DỌC Y
 	# Chỉ kích hoạt di chuyển khi đã giữ phím ui_up/ui_down đủ 1 giây
 	if abs(dir_y) > 0.1 and y_input_timer >= time_to_move_y:
 		obj.velocity.y = move_toward(obj.velocity.y, dir_y * swim_speed, 20)
@@ -66,7 +66,7 @@ func _update(delta: float) -> void:
 		else:
 			obj.velocity.y = move_toward(obj.velocity.y, 0, 10)
 
-# --- HÀM PHỤ ĐỂ TÌM MẶT NƯỚC ---
+# HÀM PHỤ ĐỂ TÌM MẶT NƯỚC 
 func get_water_surface_y() -> float:
 	var water_node = obj.get_parent().get_node_or_null("Water")
 	if water_node and water_node.has_node("CollisionShape2D"):
