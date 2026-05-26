dee28f7ff6d27c5dae2249d986386df74b00d6d7
TSang <sangdang513@gmail.com>
2026-05-26 14:46:34 +0700

    Add Aqua bubble attack
    Add BubbleAttack state, bubble projectile spawning, and attack input routing for Aqua while floating. Rename the normal attack script to attack_by_blade and clean up unused Aqua state files.

--- coregame/scenes/player/player_types/Aqua/aqua.gd
+++ coregame/scenes/player/player_types/Aqua/aqua.gd
@@ -1,11 +1,19 @@
 extends Player
 class_name AquaFox
+
 @onready var water_timer = $Timer
+@onready var bubble_factory: Node2DFactory = $Direction/BubbleFactory
+
+@export var bubble_attack_cooldown: float = 0.8
+
 var check: bool =false
 var print_time: float = 5.0
+var _bubble_cooldown_timer: float = 0.0
+
 func _update_movement(delta: float) -> void:
+	_bubble_cooldown_timer = maxf(_bubble_cooldown_timer - delta, 0.0)
 	# Bỏ qua trọng lực nếu đang ở trạng thái swim
-	if not is_on_floor() and fsm.current_state != fsm.states.swim and fsm.current_state != fsm.states.wateridle and fsm.current_state != fsm.states.float :
+	if not is_on_floor() and fsm.current_state != fsm.states.swim and fsm.current_state != fsm.states.wateridle and fsm.current_state != fsm.states.float and fsm.current_state != fsm.states.bubbleattack :
 		velocity.y += delta * gravity
 	# Nếu  on_floor đếm ngược time
 	if fsm.current_state == fsm.states.idle or fsm.current_state == fsm.states.run or fsm.current_state == fsm.states.jump or fsm.current_state == fsm.states.fall:
@@ -16,6 +24,20 @@ func _update_movement(delta: float) -> void:
 				check = true
 				water_timer.stop()
 	move_and_slide()
+
+func shoot_bubble(speed: float) -> void:
+	var bubble := bubble_factory.create() as BubbleProjectile
+	var shooting_direction := Vector2(direction * cos(deg_to_rad(60.0)), -sin(deg_to_rad(60.0)))
+	bubble.shoot(shooting_direction, speed)
+
+func can_shoot_bubble() -> bool:
+	return fsm.current_state == fsm.states.float and _bubble_cooldown_timer <= 0.0
+
+func change_to_bubble_attack() -> void:
+	if not can_shoot_bubble():
+		return
+	_bubble_cooldown_timer = bubble_attack_cooldown
+	fsm.change_state(fsm.states.bubbleattack)
 
 
 func _on_timer_timeout() -> void:
--- coregame/scenes/player/states/player_state.gd
+++ coregame/scenes/player/states/player_state.gd
@@ -81,6 +81,18 @@ func control_jump_on_water() -> bool:
 	
 func control_attack_by_blade() -> bool:
 	if Input.is_action_just_pressed('attack'):
-		change_state(fsm.states.attackbyblade)
+		if obj.has_method("change_to_bubble_attack"):
+			obj.change_to_bubble_attack()
+		else:
+			change_state(fsm.states.attackbyblade)
+		return true
+	return false
+
+func control_bubble_attack() ->bool:
+	if Input.is_action_just_pressed('attack'):
+		if obj.has_method("change_to_bubble_attack"):
+			obj.change_to_bubble_attack()
+		else:
+			change_state(fsm.states.attackbyblade)
 		return true
 	return false
