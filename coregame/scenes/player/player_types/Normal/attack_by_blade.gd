d9a94124435fc6d5d7972f06e711367026b8f60c
TSang <sangdang513@gmail.com>
2026-05-26 21:31:33 +0700

    fix attack of normal fox

--- coregame/scenes/player/player_types/Normal/attack_by_blade.gd
+++ coregame/scenes/player/player_types/Normal/attack_by_blade.gd
@@ -1,40 +1,19 @@
 extends PlayerState
-
-
 func get_hitbox():
 	return obj.get_node("Direction/HitArea2D/HitBox2D")
 
-
 func _enter() -> void:
 	timer = 0.5
-	print("attack_blade")
-
 	obj.change_animation("attackblade")
-
-	# Khi attack bắt đầu -> hitbox tắt
 	get_hitbox().set_deferred("disabled", true)
+	obj.stop_move()
 
-	# Đợi animation attack kết thúc
-	await obj.animated_sprite.animation_finished
-
-	# Bật hitbox
-	get_hitbox().set_deferred("disabled", false)
-
-	print("enabled request")
-	
-	# Chờ create_timer để detect overlap
-	await get_tree().create_timer(0.2).timeout
-
-	print("really enabled:", get_hitbox().disabled)
-
-
-	# Tắt lại
-	get_hitbox().set_deferred("disabled", true)
-
-	change_state(fsm.states.idle)
 
 func _update(delta: float) -> void:
 	if not update_timer(delta):
 		return
 	change_state(fsm.states.idle)
 	
+
+func _exit() ->void:
+	get_hitbox().set_deferred("disabled", false)
